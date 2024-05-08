67015e5b8c69b48052d45390def7dfe1bafefab6
Chumy <57281249+Jimmy01240397@users.noreply.github.com>
2024-05-08 15:31:01 +0800

    Update password.go

--- utils/password/password.go
+++ utils/password/password.go
@@ -1,43 +1,75 @@
 package password
 
 import (
+    "fmt"
+    "strings"
+    "reflect"
     "crypto/hmac"
     "crypto/sha256"
     "encoding/base64"
+    "encoding/json"
 //    "encoding/hex"
-
-    "github.com/go-errors/errors"
+    "database/sql/driver"
 
     "gintemplate/utils/config"
 )
 
-type Password struct {
-    encryptpass string
+type Password string
+
+type password struct {
+    Type string `json:"type"`
+    Password string `json:"password"`
 }
 
-func New(password string) Password {
+func New(pass string) Password {
+    if pass == "" {
+        return ""
+    }
     h := hmac.New(sha256.New, []byte(config.Secret))
-    h.Write([]byte(password))
-    return Password{
-        encryptpass: base64.StdEncoding.EncodeToString(h.Sum(nil)),
+    h.Write([]byte(pass))
+    return Password(base64.StdEncoding.EncodeToString(h.Sum(nil)))
+}
+
+func (c Password) MarshalJSON() ([]byte, error) {
+    pass := password{
+        Type: "secret",
+        Password: string(c),
     }
+    return json.Marshal(pass)
 }
 
-func NewEncrypt(encryptpass string) Password {
-    return Password{
-        encryptpass: string(encryptpass),
+func (c *Password) UnmarshalJSON(b []byte) error {
+    var tmpstr string
+    err := json.Unmarshal(b, &tmpstr)
+    if err == nil {
+        *c = New(tmpstr)
+        return err
     }
+    var tmppass password
+    err = json.Unmarshal(b, &tmppass)
+    if err != nil {
+        return err
+    }
+    switch strings.ToLower(tmppass.Type) {
+    case "plain":
+        *c = New(tmppass.Password)
+    case "secret":
+        *c = Password(tmppass.Password)
+    default:
+        return fmt.Errorf("Invalid type %s", strings.ToLower(tmppass.Type))
+    }
+    return nil
 }
 
-func (pass Password) String() string {
-    return pass.encryptpass
-}
-
-func (pass *Password) Scan(src any) (err error) {
-    var ok bool
-    pass.encryptpass, ok = src.(string)
-    if !ok {
-        err = errors.New("Bad password type")
+func (c *Password) Scan(value interface{}) (err error) {
+    if val, ok := value.(string); ok {
+        *c = Password(val)
+    } else {
+        err = fmt.Errorf("sql: unsupported type %s", reflect.TypeOf(value))
     }
     return
+}
+
+func (c Password) Value() (driver.Value, error) {
+    return string(c), nil
 }
